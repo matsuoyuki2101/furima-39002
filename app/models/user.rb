806c6eeb22539a7cbcc83cfc91866d7fdb9dcc82
yuki1102anata <tnmm.ereviv@docomo.ne.jp>
2023-07-01 20:22:28 +0900

    userテストコードと正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,11 +14,11 @@ class User < ApplicationRecord
           # @含むこと・存在することはdeviseのデフォルト実装のため省略
 
           # 全角ひらがな、全角カタカナ、漢字
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々一]+\z/}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々一]+\z/}
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
           # 全角カタカナ
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー-龥々－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー-龥々－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
     validates :birthday
   end
 end
