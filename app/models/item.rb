6669a118580ff205cf03f5a71e4f83016bf728a9
yuki1102anata <tnmm.ereviv@docomo.ne.jp>
2023-07-05 17:35:13 +0900

    アソシエーション、バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
    has_one_attached :image
   
    with_options presence: true do
-   validates :user_id
+  
    validates :image
    validates :name
    validates :description
@@ -28,8 +28,8 @@ class Item < ApplicationRecord
    validates :prefecture_id
    validates :shipping_date_id
    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-   validates :price, format: { with: /\A[0-9]+\z/ }
+   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, is_not_a_number: '９９９'}
+   
    end
   
   # ジャンルの選択が「--」の時は保存不可
