782e36efa15fa7fcfe05c4624e58fbc50c238b42
yuki1102anata <tnmm.ereviv@docomo.ne.jp>
2023-07-06 19:14:04 +0900

    商品詳細ページでの条件分岐を実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :non_purchased_item, only: [:index, :create]
+  
+  def index
+    @order_form = OrderForm.new
+  end
+  
+  def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+end
