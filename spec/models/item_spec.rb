bc5ae5a2a50f5ad3ef83a7ac1cf20454cecb8883
yuki1102anata <tnmm.ereviv@docomo.ne.jp>
2023-08-13 20:42:17 +0900

    itemフォームテストを英語へ修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -45,22 +45,22 @@ RSpec.describe Item, type: :model do
       it '１枚画像がないと出品できない' do
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品画像を入力してください")
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品名が空欄だと出品できない' do
         @item.name = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品名を入力してください")
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it '商品の説明が空欄だと出品できない' do
         @item.description = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
+        expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリーの情報が「---」だと出品できない' do
         @item.category_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('カテゴリーは0以外の値にしてください')
+        expect(@item.errors.full_messages).to include('Category must be other than 0')
       end
       it 'カテゴリーの情報が空欄だと出品できない' do
         @item.category_id = nil
@@ -70,7 +70,7 @@ RSpec.describe Item, type: :model do
       it '商品の状態の情報が「---」だと出品できない' do
         @item.item_status_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('商品の状態は0以外の値にしてください')
+        expect(@item.errors.full_messages).to include('Item status must be other than 0')
       end
       it '商品の状態の情報が空欄だと出品できない' do
         @item.item_status_id = nil
@@ -80,7 +80,7 @@ RSpec.describe Item, type: :model do
       it '配送料の負担の情報が「---」だと出品できない' do
         @item.shipping_cost_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('配送料をどちらが負担するかは0以外の値にしてください')
+        expect(@item.errors.full_messages).to include('Shipping cost must be other than 0')
       end
       it '配送料の負担の情報が空欄だと出品できない' do
         @item.shipping_cost_id = nil
@@ -100,17 +100,17 @@ RSpec.describe Item, type: :model do
       it '発送までの日数の情報が「---」だと出品できない' do
         @item.shipping_date_id = 0
         @item.valid?
-        expect(@item.errors.full_messages).to include('発送までの日数は0以外の値にしてください')
+        expect(@item.errors.full_messages).to include('Shipping date must be other than 0')
       end
       it '発送までの日数の情報が空欄だと出品できない' do
         @item.shipping_date_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("発送までの日数を入力してください")
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
       end
       it '価格が空欄だと出品できない' do
         @item.price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("価格を入力してください")
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格の範囲が、300円未満だと出品できない' do
         @item.price = 100
